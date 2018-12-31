1bd212b7af3e9ade6244eb4ca5fc630c593f42dc
chisco <chisquin_francisco@hotmail.com>
2018-12-30 22:30:33 -0600

    se agrega metodo saludar

--- src/Run.java
+++ src/Run.java
@@ -4,5 +4,9 @@ public class Run {
 	public static void main(String args []) {
 		System.out.println("mi clase principal");
 	}
+	
+	public String saludar(String nombre){
+		return "hola " + nombre;		
+	}
 
 }
